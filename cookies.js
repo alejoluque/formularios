0238e6aae34dd5ba5a0460068985b1a13aa4141b
Alejandro Alvarez luque <65513462+alejoluque@users.noreply.github.com>
2023-04-26 09:37:27 -0500

    Create cookies.js

---
+++ cookies.js
@@ -0,0 +1,24 @@
+<script>
+function getCookie(cname) {
+  let name = cname + "=";
+  let ca = document.cookie.split(';');
+  for(let i = 0; i < ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
+var agencia = getCookie('agencia');
+	
+if(agencia===""){    
+}else{
+    var elementSource= document.getElementById("agencia");
+    if(elementSource == null){}else{document.getElementById("agencia").value = agencia;}
+}
+	
+</script>
